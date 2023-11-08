7276ce47ee3a7233361bb87f92235719fefddadc
Chatis Santos <chatis@afrointelligence.com>
2023-11-08 17:20:24 -0500

    updated api url to aws

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
-export const baseUrl = 'http://localhost:4089/ask';
-// export const baseUrl = 'https://purefitapi.procamp.dev/ask';
+// export const baseUrl = 'http://localhost:4089/ask';
+export const baseUrl = 'https://purefitapi.procamp.dev/ask';
 
 export const skillGapAnalysisPath = '/skill-gap-analysis';
 export const coverLetterPath = '/cover-letter';
