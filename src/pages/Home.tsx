092e3052b0eb3d7f624c0d4fc22ed68e41b12985
Chatis Santos <chatis@afrointelligence.com>
2023-11-08 17:20:52 -0500

    updated home page copy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center p-4">
+    <div className="min-h-screen mt-16 flex flex-col justify-center items-center p-4">
       <div className="w-full max-w-xl bg-white rounded-lg shadow-md p-6">
-        <h1 className="text-2xl font-bold mb-4">Welcome to Helphopper!</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Welcome to Helphopper!
+        </h1>
         <p className="text-gray-700 mb-4">
           Helphopper boosts your job application process. Here's how to get
           started:
         </p>
         <ol className="list-decimal list-inside mb-4">
-          <li className="mb-2">Upload or enter your resume.</li>
-          <li className="mb-2">Input the desired job description.</li>
-          <li className="mb-2">Manually trigger the skill gap analysis.</li>
-          <li>Receive a tailored cover letter based on your input.</li>
+          <li className="mb-2">Upload a resume.</li>
+          <li className="mb-2">Upload a job description.</li>
+          <li className="mb-2">Generate skill gap analysis or cover letter</li>
         </ol>
         <p className="text-gray-700 mb-4">
           Ready to leap forward in your job hunt? Let's get started!
